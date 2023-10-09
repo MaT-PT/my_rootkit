c50e608f9e42d597747ead078bda855922d6576a
MaT-PT <MaT-PT@users.noreply.github.com>
2023-10-09 18:40:40 +0200

    Include my local VSCode settings for consistent coding

--- scripts/tests/test.c
+++ scripts/tests/test.c
@@ -2,18 +2,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main() {
-    int fd;
-    char data[256];
-    const char text[] = "Hello, world!\n";
+int main()
+{
+	int fd;
+	char data[256];
+	const char text[] = "Hello, world!\n";
 
-    fd = open("test.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    write(fd, text, sizeof(text));
-    close(fd);
+	fd = open("test.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	write(fd, text, sizeof(text));
+	close(fd);
 
-    fd = open("test.txt", O_RDONLY);
-    read(fd, data, sizeof(data));
-    close(fd);
+	fd = open("test.txt", O_RDONLY);
+	read(fd, data, sizeof(data));
+	close(fd);
 
-    return 0;
+	return 0;
 }
